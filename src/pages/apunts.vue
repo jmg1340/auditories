16c7fd4def6ace7685fc64a659f1c4ee9d9613f7
jordi <jordi.miserachs@gmail.com>
2021-03-08 14:56:01 +0100

    APUNTS 2

--- src/pages/apunts.vue
+++ src/pages/apunts.vue
@@ -4,6 +4,17 @@
 
 		<q-card class="q-ma-md">
 			<q-card-section>
+				<div class="text-h6">Con relación a las verificaciones (y correspondientes no conformidades si las hay) de los códigos de alarma genéricos:</div>
+					<ul>
+						<li>indicar que éstas deben hacer referencia única y exclusivamente al personal EXTERNO (M-1450). Por el momento este control no debe ser enfocado ni tratado desde la perspectiva de los usuarios internos (pendiente de desarrollo).</li>
+						<li>En cuanto se hayan consensuado los criterios y éstos estén por escrito en forma de manual serán objeto de auditoría o verificación. </li>
+						<li>Aquellas ACP que estén pendientes (y afecten a personal propio) pueden ser cerradas indicando que para este colectivo se está desarrollando el oportuno procedimiento.</li>
+					</ul>
+			</q-card-section>
+		</q-card>
+
+		<q-card class="q-ma-md">
+			<q-card-section>
 				<div class="text-h6">Eliminar las siguientes frases de la plantilla del informe:</div>
 					<ul>
 						<li>"Las No Conformidades han sido aclaradas y entendidas".</li>
